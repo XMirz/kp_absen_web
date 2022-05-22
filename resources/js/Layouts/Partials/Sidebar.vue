e4eaf68323a01e27caa10c29e78fa2cdba0ce5e8
xmirz <linearattack0@gmail.com>
2022-05-23 00:49:19 +0700

    added setting vue view, sidebar need to be fixed

--- resources/js/Layouts/Partials/Sidebar.vue
+++ resources/js/Layouts/Partials/Sidebar.vue
@@ -1,35 +1,59 @@
 <script setup>
+import { computed } from "vue";
 import NavLink from "@/Components/NavLink.vue";
+import { usePage } from "@inertiajs/inertia-vue3";
+const { user } = computed(() => usePage().props);
+const avatarUrl = computed(
+  () =>
+    "https://ui-avatars.com/api/?background=0D8ABC&color=fff&size=96&name=" +
+    "as"
+);
 </script>
 <template>
   <aside
     class="bg-white border-black/15 shadow-black/10 shadow-md border-r w-72 py-8 !h-screen font-inter">
     <div class="flex h-full flex-col">
-      <div class="flex-1 px-2 font-medium space-y-2 w-full">
-        <NavLink
-          :href="route('dashboard')"
-          :active="route().current('*dashboard*')">
-          Dasbor</NavLink
-        >
-        <NavLink
-          :href="route('staffs.index')"
-          :active="route().current('*staff*')">
-          Pegawai</NavLink
-        >
-        <NavLink
-          :href="route('staffs.index')"
-          :active="route().current('*events*')">
-          Jadwal Kegiatan</NavLink
-        >
-        <NavLink
-          :href="route('staffs.index')"
-          :active="route().current('*presence*')">
-          Absensi</NavLink
-        >
+      <div class="flex-1 space-y-6">
+        <div class="px-2 flex flex-row">
+          <img class="rounded-full w-14 h-14" alt="" :src="avatarUrl" />
+          <div class="">
+            <h1>{{}}</h1>
+          </div>
+        </div>
+        <div class="px-2 font-medium space-y-2 w-full">
+          <NavLink
+            :href="route('dashboard')"
+            :active="route().current('*dashboard*')">
+            Dasbor</NavLink
+          >
+          <NavLink
+            :href="route('staffs.index')"
+            :active="route().current('*staff*')">
+            Pegawai</NavLink
+          >
+          <NavLink
+            :href="route('staffs.index')"
+            :active="route().current('*events*')">
+            Jadwal Kegiatan</NavLink
+          >
+          <NavLink
+            :href="route('staffs.index')"
+            :active="route().current('*presence*')">
+            Absensi</NavLink
+          >
+          <NavLink
+            :href="route('settings.index')"
+            :active="route().current('*settings*')">
+            Pengaturan</NavLink
+          >
+          {{ user }}
+          <!-- {{ usePage().props.value.auth.user }} -->
+        </div>
       </div>
+      <!-- Bottom -->
       <div class="font-poppins px-4 space-x-4 flex flex-row">
         <img
-          src="{{ asset('images/logo_kota_pekanbaru.png') }}"
+          :src="'images/logo_kota_pekanbaru.png'"
           alt="Lambang kota pekanbaru"
           class="w-12 object-contain mx-auto" />
         <div class="">
@@ -42,3 +66,4 @@ import NavLink from "@/Components/NavLink.vue";
     </div>
   </aside>
 </template>
+<script></script>
